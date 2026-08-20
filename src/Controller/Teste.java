4f768936ad14189b8319fee5611b0a75513b1b5a
Luis Nhabangue <luisnhabangue@uem.ac.mz>
2026-08-20 16:11:15 +0200

    Add additional print statement in Teste class

--- src/Controller/Teste.java
+++ src/Controller/Teste.java
@@ -5,6 +5,7 @@ import org.w3c.dom.ls.LSOutput;
 public class Teste{
     static void main(String[] args) {
         System.out.println("Ola Mundo");
+        System.out.println("Teste 2");
     }
 
 
